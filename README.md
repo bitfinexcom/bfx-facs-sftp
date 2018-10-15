6d8f90da4720ab44402b95b35f4b0df50fc856cf
jacobplaster <jacob.s.plaster@gmail.com>
2018-10-15 15:07:47 +0100

    sftp.js: creates an sftp connection on start

--- README.md
+++ README.md
@@ -9,3 +9,93 @@ git remote add upstream https://github.com/bitfinexcom/PARENT.git
 git remote -v
 
 git push origin master
+
+## Example config
+
+```
+{
+  host: '127.0.1',
+  port: '22',
+  username: 'bitfinex',
+  password: '1234'
+}
+```
+
+
+## Example Usage
+Please make sure that your SFTP server has root access to its
+directory.
+
+#### Create directory
+
+```
+this.sftp.mkdir('./hello_world_2')
+  .then(console.log)
+
+// undefined
+```
+
+#### List directory
+
+```
+this.sftp.list('/')
+  .then(console.log)
+
+/*
+[ { type: 'd',
+    name: 'hello_world',
+    size: 0,
+    modifyTime: 1539607160000,
+    accessTime: 1539607160000,
+    rights: { user: 'rw', group: 'rw', other: 'rw ' },
+    owner: 0,
+    group: 0 },
+  { type: 'd',
+    name: 'hello_world_2',
+    size: 0,
+    modifyTime: 1539608708000,
+    accessTime: 1539608708000,
+    rights: { user: 'rw', group: 'rw', other: 'rw ' },
+    owner: 0,
+    group: 0 } ]
+*/
+```
+
+#### Fast put file
+
+```
+this.sftp.fastPut('./README.md', './hello_worlds')
+  .then(console.log)
+
+// ./README.md was successfully uploaded to ./hello_worlds!
+```
+
+#### Fast get file
+
+```
+this.sftp.fastGet('./hello_worlds', './hellow_worlds')
+  .then(console.log)
+
+// ./hello_worlds was successfully download to ./hellow_worlds!
+```
+
+#### Delete file
+
+```
+this.sftp.delete('./hello_worlds')
+  .then(console.log)
+
+// undefined
+```
+
+#### Chmod
+
+```
+this.sftp.chmod('./hello_world', 700)
+  .then(console.log)
+
+// undefined
+```
+
+For more info on how to use the this.sftp module, refer to the ssh2-this.sftp-client docs:
+https://github.com/jyu213/ssh2-this.sftp-client
--- sftp.js
+++ sftp.js
@@ -3,57 +3,61 @@
 const async = require('async')
 const _ = require('lodash')
 const Base = require('bfx-facs-base')
+let Client = require('ssh2-sftp-client')
 
 function client (conf, label) {
   // create sft object
-
-  return null
+  this.sftp = new Client()
+  return this.sftp.connect({
+    host: conf.host,
+    port: conf.port,
+    username: conf.username,
+    password: conf.password
+  })
 }
 
 class StoreFacility extends Base {
   constructor (caller, opts, ctx) {
     super(caller, opts, ctx)
-      this.name = 'sftp'
+    this.name = 'sftp'
     this._hasConf = true
-      this.init()
+    this.init()
   }
-    _start (cb) {
+  _start (cb) {
     async.series([
       next => { super._start(next) },
       next => {
-        const conf = _.pick(this.conf, ['repo', 'secret'])
-          const {
-          repo,
-          secret
+        const conf = _.pick(this.conf, ['host', 'port', 'username', 'password'])
+        const {
+          host,
+          port,
+          username,
+          password
         } = conf
-          if (!repo || !secret) {
+        if (!host || !port || !username || !password) {
           return next(
-            new Error('Repo or secret is missing in config')
+            new Error('Host, port, username or password is missing in config')
           )
         }
-        this.sftp = client(conf)
-        // this.ipfs.on('ready', () => {
-        //   this.ipfs_initialised = true
-        //   this.ipfs.start()
-        //     .then(() => {
-        //       next(null)
-        //     })
-        //     .catch((err) => next(err))
-        // })
+        client(conf)
+          .then(() => {
+            next(null)
+          })
+          .catch((err) => {
+            next(err)
+          })
       }
     ], cb)
   }
-    _stop (cb) {
-    async.series([
+  _stop (cb) {
+    this.sftp.async.series([
       next => { super._stop(next) },
       next => {
-        // if (this.ipfs_initialised) {
-        //   this.ipfs.stop()
-        //     .catch((err) => next(err))
-        // }
-        // delete this.ipfs
-        // delete this.ipfs_initialised
-        next()
+        this.sftp.end()
+        this.sftp.on('end', () => {
+          delete this.sftp
+          next()
+        })
       }
     ], cb)
   }
