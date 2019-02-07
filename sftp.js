31b8859a397a3062bd197382d70ebeab92f82e3c
prdn <paolo.ardoino@gmail.com>
2019-02-07 17:05:16 +0000

    fix

--- sftp.js
+++ sftp.js
@@ -8,37 +8,50 @@ let Client = require('ssh2-sftp-client')
 function client (conf, label) {
   // create sft object
   this.sftp = new Client()
+
   return this.sftp.connect({
     host: conf.host,
     port: conf.port,
     username: conf.username,
-    password: conf.password
+    password: conf.password,
+    privateKey: conf.privateKey
   })
 }
 
 class StoreFacility extends Base {
   constructor (caller, opts, ctx) {
     super(caller, opts, ctx)
+
     this.name = 'sftp'
     this._hasConf = true
+
     this.init()
   }
+
   _start (cb) {
     async.series([
       next => { super._start(next) },
       next => {
-        const conf = _.pick(this.conf, ['host', 'port', 'username', 'password'])
+        const conf = _.pick(this.conf, [
+          'host', 'port',
+          'username', 'password',
+          'privateKey'
+        ])
+
         const {
           host,
           port,
           username,
-          password
+          password,
+          privateKey
         } = conf
-        if (!host || !port || !username || !password) {
+
+        if (!host || !port || !username || !(password || privateKey) {
           return next(
             new Error('Host, port, username or password is missing in config')
           )
         }
+
         client(conf)
           .then(() => {
             next(null)
@@ -49,6 +62,7 @@ class StoreFacility extends Base {
       }
     ], cb)
   }
+
   _stop (cb) {
     this.sftp.async.series([
       next => { super._stop(next) },
